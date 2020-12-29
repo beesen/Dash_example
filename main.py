5d3852f16b2c16fa4e486a15a3a9c089c9868b98
beesen <beesen@uvt.nl>
2020-12-29 12:44:38 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,173 @@
+import pandas as pd
+import plotly.express as px  # (version 4.7.0)
+import sqlite3
+import dash  # (version 1.12.0) pip install dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from datetime import datetime
+
+app = dash.Dash(__name__,
+                external_stylesheets=[dbc.themes.BOOTSTRAP],
+                meta_tags=[{
+                    "name": "viewport",
+                    "content": "width=device-width, initial-scale=1"},
+                ],)
+
+
+# ------------------------------------------------------------------------------
+# Return data as HTML table
+def generate_table(dataframe: pd.DataFrame, max_rows: int = 10) -> str:
+    """
+    :param dataframe: dataframe to show
+    :param max_rows: max number of rows to display
+    :return: the HTML table
+    """
+    table_header = []
+    for col in dataframe.columns:
+        table_header.append(html.Th(col))
+    table_header = [html.Thead(html.Tr(table_header))]
+
+    table_body = []
+    for i in range(min(len(dataframe), max_rows)):
+        row = []
+        for col in dataframe.columns:
+            row.append(html.Td(dataframe.iloc[i][col]))
+        table_body.append(html.Tr(row))
+    table_body = [html.Tbody(table_body)]
+
+    return dbc.Table(table_header + table_body)
+
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+
+# -------------------------------------------------------------------------------
+# Calculate age from data frame df_birth_date
+def create_age_df(df_birth_date: pd.DataFrame) -> pd.DataFrame:
+    df_age = df_birth_date.apply(
+        lambda x: (datetime.now().date() - datetime(int(x[:4]), int(x[5:7]),
+                                                    int(x[8:10])).date()) / 365.2425)
+    #    # seems odd to use .days...
+    return df_age.apply(lambda x: x.days)
+
+
+# ------------------------------------------------------------------------------
+# Import and clean data (importing csv into pandas)
+db_con = sqlite3.connect('data.sqlite3')
+db_cur = db_con.cursor()
+
+sql = f'select * from respondents'
+rows = db_cur.execute(sql)
+columns = []
+for column in db_cur.description:
+    columns.append(column[0].lower())
+df = pd.DataFrame(rows, columns=columns)
+db_con.close()
+
+df['age'] = create_age_df(df['birth_date'])
+# print(df.info())
+# print(df.head(10))
+
+# ------------------------------------------------------------------------------
+#
+def bld_options(df):
+    """
+    Build list of dicts from dataframe column names
+    :param df:
+    :return: HTML select as string
+    """
+    cols = df.columns.tolist()
+    options = []
+    for col in cols:
+        options.append({"label": col, "value": col})
+    return options
+
+
+# ------------------------------------------------------------------------------
+# App layout
+app.layout = html.Div([
+        dbc.Row(dbc.Col(html.H3('NPM Respondents Dash', style={'textAlign': 'center'}),
+                        width={'size': 6, 'offset': 3},
+                        ),
+                ),
+        dbc.Row(dbc.Col(generate_table(df), width={'size': 8, 'offset': 2},)
+                ),
+        dbc.Row(
+            [
+                dbc.Col(dbc.Label("Select"),
+                        width=1),
+                dbc.Col(dcc.Dropdown(id="select_option",
+                 options=bld_options(df),
+                 multi=False,
+                 value="sexe",
+                 ),
+                width=2),
+                dbc.Col(dbc.Label("Select"),
+                width={'size': 1, 'offset': 1}),
+                dbc.Col(dcc.Dropdown(id="select_option2",
+                 options=bld_options(df),
+                 multi=False,
+                 value="sexe",
+                 ),
+                width=2),
+            ], no_gutters=False
+        ),
+        dbc.Row(
+            [
+                dbc.Col(dcc.Graph(id='respondents_map', figure={}),
+                        width=8, lg={'size': 6,  "offset": 3, 'order': 'first'}
+                        ),
+            ]
+        )
+])
+""""
+app.layout = html.Div([
+    html.H1('NPM Respondents Dash', style={'textAlign': 'center'}),
+    generate_table(df),
+    dcc.Dropdown(id="select_option",
+                 options=bld_options(df),
+                 multi=False,
+                 value="sexe",
+                 style={'width': "40%"}
+                 ),
+    dcc.Dropdown(id="select_option2",
+                 options=bld_options(df),
+                 multi=False,
+                 value="sexe",
+                 style={'width': "40%"}
+                 ),
+    dcc.Graph(id='respondents_map', figure={})
+])
+"""
+
+# ------------------------------------------------------------------------------
+# Connect the Plotly graphs with Dash Components
+@app.callback(
+    Output(component_id='respondents_map', component_property='figure'),
+    [
+        Input('select_option', 'value'),
+        Input('select_option2', 'value')
+    ])
+def update_graphs(option_selected, option2_selected):
+    fig = px.histogram(df,  # dataframe
+                       x=option_selected,  # x-values column
+                       #                   nbins=10,        #number of bins
+                       color=option2_selected
+                       )
+    # Plotly Express
+    return fig  # , pv
+
+
+# ------------------------------------------------------------------------------
+if __name__ == '__main__':
+    app.run_server(debug=True)
